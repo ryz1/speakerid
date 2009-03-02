ea83b841ab343b8c5fce9a82a200920a727c2ab1
carlosynoguti <carlosynoguti@b71da484-0734-11de-9036-69e38a880166>
2009-03-02 18:45:16 +0000

    apagando tudo

--- gmm/showgmm.h
+++
@@ -1,8 +0,0 @@
-// Function that shows the components of a GMM on the screen
-
-#ifndef SHOWGMM_H_
-#define SHOWGMM_H_
-
-void showgmm(struct mixture *l, int nGaussians, int dim);
-
-#endif /*SHOWGMM_H_*/
